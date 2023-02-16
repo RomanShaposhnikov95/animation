c736d16054227bf6d6c2e703ee3fafbd66e2096a
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-02-16 02:23:06 +0200

    first commit

---
+++ index_files/init.php
@@ -0,0 +1,9 @@
+window[mtsdk.globalName]('set', 'websiteId', '06e9100b-c82f-4507-985e-1f2cad320c69');
+window[mtsdk.globalName]('set', 'country', 'lv');
+window[mtsdk.globalName]('set', 'revision', 1676469688);
+window[mtsdk.globalName]('set', 'device', 'desktop');
+window[mtsdk.globalName]('set', 'pageview', 2);
+window[mtsdk.globalName]('set', 'send_ga', 1);
+window[mtsdk.globalName]('set', 'language', 'en');
+window[mtsdk.globalName]('setVisit');
+window[mtsdk.globalName]('initCampaigns', '[]');
---
+++ index_files/page_data/sEG3UK0UGbm.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("SkywalkerHelpers",[],(function(a,b,c,d,e,f){"use strict";function a(a){a=a.split("/").slice(0,-1);return a.some(function(a){return/\d/.test(a)})?null:a.join("_")}f.topicToUseCaseName=a}),66);
